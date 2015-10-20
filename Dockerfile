6b7c5ed7ccd808921e77cfcfe049d026381647d0
wwj718 <wuwenjie718@gmail.com>
2015-10-20 16:57:40 +0800

    install django-extensions

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,16 @@
 # This is a tweaked version of https://github.com/appsembler/configuration/wiki/Creating-a-Docker-image 
 #
 
-FROM wwj718/edx_cypress_docker:1.11
+FROM wwj718/edx_cypress_docker:1.12
 MAINTAINER wwj718 <wuwenjie718@gmail.com>
 #user root:edx
 
 #ADD config/docker.py /edx/app/edxapp/edx-platform/lms/envs/
 #RUN /bin/chown edxapp.edxapp /edx/app/edxapp/edx-platform/lms/envs/docker.py
-WORKDIR /edx/app/edxapp/edx-platform
-RUN grep -rl 'ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js' ./ | xargs sed -i 's#ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js#cdn.bootcss.com/jquery/1.10.1/jquery.min.js#g'
-WORKDIR /edx/app/edxapp/venvs/edxapp/lib/python2.7/site-packages
-RUN grep -rl 'ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js' ./ | xargs sed -i 's#ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js#cdn.bootcss.com/jquery/2.1.0/jquery.min.js#g'
 #add 
+RUN /edx/app/edxapp/venvs/edxapp/bin/pip install Werkzeug
+RUN /edx/app/edxapp/venvs/edxapp/bin/pip install django-extensions
+ADD config/devstack.py /edx/app/edxapp/edx-platform/lms/envs/
 #ADD config/wwjtest.py /tmp/
 
 #CMD /usr/sbin/sshd -D&/sbin/my_init --enable-insecure-key
---
+++ config/devstack.py
@@ -0,0 +1,195 @@
+"""
+Specific overrides to the base prod settings to make development easier.
+"""
+
+from .aws import *  # pylint: disable=wildcard-import, unused-wildcard-import
+
+# Don't use S3 in devstack, fall back to filesystem
+del DEFAULT_FILE_STORAGE
+MEDIA_ROOT = "/edx/var/edxapp/uploads"
+
+
+DEBUG = True
+USE_I18N = True
+TEMPLATE_DEBUG = True
+SITE_NAME = 'localhost:8000'
+PLATFORM_NAME = ENV_TOKENS.get('PLATFORM_NAME', 'Devstack')
+# By default don't use a worker, execute tasks as if they were local functions
+CELERY_ALWAYS_EAGER = True
+
+################################ LOGGERS ######################################
+
+import logging
+
+# Disable noisy loggers
+for pkg_name in ['track.contexts', 'track.middleware', 'dd.dogapi']:
+    logging.getLogger(pkg_name).setLevel(logging.CRITICAL)
+
+
+################################ EMAIL ########################################
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+FEATURES['ENABLE_INSTRUCTOR_EMAIL'] = True     # Enable email for all Studio courses
+FEATURES['REQUIRE_COURSE_EMAIL_AUTH'] = False  # Give all courses email (don't require django-admin perms)
+
+
+########################## ANALYTICS TESTING ########################
+
+ANALYTICS_SERVER_URL = "http://127.0.0.1:9000/"
+ANALYTICS_API_KEY = ""
+
+# Set this to the dashboard URL in order to display the link from the
+# dashboard to the Analytics Dashboard.
+ANALYTICS_DASHBOARD_URL = None
+
+
+################################ DEBUG TOOLBAR ################################
+
+INSTALLED_APPS += ('debug_toolbar', 'debug_toolbar_mongo','django_extensions')
+MIDDLEWARE_CLASSES += (
+    'django_comment_client.utils.QueryCountDebugMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar_mongo.panel.MongoDebugPanel',
+    # ProfilingPanel has been intentionally removed for default devstack.py
+    # runtimes for performance reasons. If you wish to re-enable it in your
+    # local development environment, please create a new settings file
+    # that imports and extends devstack.py.
+)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': 'lms.envs.devstack.should_show_debug_toolbar'
+}
+
+
+def should_show_debug_toolbar(_):
+    return True  # We always want the toolbar on devstack regardless of IP, auth, etc.
+
+
+########################### PIPELINE #################################
+
+PIPELINE_SASS_ARGUMENTS = '--debug-info --require {proj_dir}/static/sass/bourbon/lib/bourbon.rb'.format(proj_dir=PROJECT_ROOT)
+
+########################### VERIFIED CERTIFICATES #################################
+
+FEATURES['AUTOMATIC_VERIFY_STUDENT_IDENTITY_FOR_TESTING'] = True
+FEATURES['ENABLE_PAYMENT_FAKE'] = True
+
+CC_PROCESSOR_NAME = 'CyberSource2'
+CC_PROCESSOR = {
+    'CyberSource2': {
+        "PURCHASE_ENDPOINT": '/shoppingcart/payment_fake/',
+        "SECRET_KEY": 'abcd123',
+        "ACCESS_KEY": 'abcd123',
+        "PROFILE_ID": 'edx',
+    }
+}
+
+########################### External REST APIs #################################
+FEATURES['ENABLE_OAUTH2_PROVIDER'] = True
+OAUTH_OIDC_ISSUER = 'http://127.0.0.1:8000/oauth2'
+FEATURES['ENABLE_MOBILE_REST_API'] = True
+FEATURES['ENABLE_VIDEO_ABSTRACTION_LAYER_API'] = True
+
+########################## SECURITY #######################
+FEATURES['ENFORCE_PASSWORD_POLICY'] = False
+FEATURES['ENABLE_MAX_FAILED_LOGIN_ATTEMPTS'] = False
+FEATURES['SQUELCH_PII_IN_LOGS'] = False
+FEATURES['PREVENT_CONCURRENT_LOGINS'] = False
+FEATURES['ADVANCED_SECURITY'] = False
+PASSWORD_MIN_LENGTH = None
+PASSWORD_COMPLEXITY = {}
+
+
+########################### Milestones #################################
+FEATURES['MILESTONES_APP'] = True
+
+
+########################### Entrance Exams #################################
+FEATURES['ENTRANCE_EXAMS'] = True
+
+################################ COURSE LICENSES ################################
+FEATURES['LICENSING'] = True
+
+
+########################## Courseware Search #######################
+FEATURES['ENABLE_COURSEWARE_SEARCH'] = False
+SEARCH_ENGINE = "search.elastic.ElasticSearchEngine"
+
+
+########################## Dashboard Search #######################
+FEATURES['ENABLE_DASHBOARD_SEARCH'] = True
+
+
+########################## Certificates Web/HTML View #######################
+FEATURES['CERTIFICATES_HTML_VIEW'] = True
+
+
+########################## Course Discovery #######################
+from django.utils.translation import ugettext as _
+LANGUAGE_MAP = {'terms': {lang: display for lang, display in ALL_LANGUAGES}, 'name': _('Language')}
+COURSE_DISCOVERY_MEANINGS = {
+    'org': {
+        'name': _('Organization'),
+    },
+    'modes': {
+        'name': _('Course Type'),
+        'terms': {
+            'honor': _('Honor'),
+            'verified': _('Verified'),
+        },
+    },
+    'language': LANGUAGE_MAP,
+}
+
+FEATURES['ENABLE_COURSE_DISCOVERY'] = False
+FEATURES['COURSES_ARE_BROWSEABLE'] = True
+HOMEPAGE_COURSE_MAX = 9
+
+# Software secure fake page feature flag
+FEATURES['ENABLE_SOFTWARE_SECURE_FAKE'] = True
+
+# Setting for the testing of Software Secure Result Callback
+VERIFY_STUDENT["SOFTWARE_SECURE"] = {
+    "API_ACCESS_KEY": "BBBBBBBBBBBBBBBBBBBB",
+    "API_SECRET_KEY": "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC",
+}
+
+# Skip enrollment start date filtering
+SEARCH_SKIP_ENROLLMENT_START_DATE_FILTERING = True
+
+
+########################## Shopping cart ##########################
+FEATURES['ENABLE_SHOPPING_CART'] = True
+FEATURES['STORE_BILLING_INFO'] = True
+FEATURES['ENABLE_PAID_COURSE_REGISTRATION'] = True
+FEATURES['ENABLE_COSMETIC_DISPLAY_PRICE'] = True
+
+########################## Third Party Auth #######################
+
+if FEATURES.get('ENABLE_THIRD_PARTY_AUTH') and 'third_party_auth.dummy.DummyBackend' not in AUTHENTICATION_BACKENDS:
+    AUTHENTICATION_BACKENDS = ['third_party_auth.dummy.DummyBackend'] + list(AUTHENTICATION_BACKENDS)
+
+#####################################################################
+# See if the developer has any local overrides.
+try:
+    from .private import *      # pylint: disable=wildcard-import
+except ImportError:
+    pass
+
+#####################################################################
+# Lastly, run any migrations, if needed.
+MODULESTORE = convert_module_store_setting_if_needed(MODULESTORE)
+
+SECRET_KEY = '85920908f28904ed733fe576320db18cabd7b6cd'
